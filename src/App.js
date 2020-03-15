d23fa849ab9249ec87029bbd320d6dad1d4468b2
Truong <truongnguyen1582000@gmail.com>
2020-03-15 14:31:42 +0700

    add dynamic classname and install classNames module

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Todoitem from "./components/Todoitem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Todoitem title="Go home" />
-        <Todoitem title="Rest" />
-        <Todoitem title="Eat" />
-        <p>
-          Edit <code> src / App.js </code> and save to reload.{" "}
-        </p>{" "}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React{" "}
-        </a>{" "}
-      </header>{" "}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      { title: "Go home", isComplete: true },
+      { title: "Rest", isComplete: false },
+      { title: "Eat", isComplete: true }
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.todoItems.map((item, index) => (
+            <Todoitem key={index} item={item} />
+          ))}
+        </header>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-import App from "../App";
+import "./Todoitem.css";
+var classNames = require("classnames");
 
 class Todoitem extends Component {
   render() {
+    const { item } = this.props;
+    var className = classNames("Todoitem", {
+      "Todoitem-complete": item.isComplete
+    });
     return (
-      <div className="Todoitem">
-        <p>{this.props.title}</p>
+      <div className={className}>
+        <p>{item.title}</p>
       </div>
     );
   }
